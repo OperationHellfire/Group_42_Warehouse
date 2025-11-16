81975c9dd0c67d7d1997093afdc299cfab60b968
SMurat2141 <serkan22095377@gmail.com>
2025-11-16 20:52:16 +0100

    Add files via upload

---
+++ G42Warehouse/Enum.cs
@@ -0,0 +1,26 @@
+﻿namespace G42Warehouse.Domain
+{
+    public enum SectionStatus
+    {
+        Active,
+        Maintenance,
+        Closed
+    }
+
+    public enum ExperienceLevel
+    {
+        Junior = 1,
+        Mid = 2,
+        Senior = 3
+    }
+
+    public enum HazardType
+    {
+        Toxic,
+        Flammable,
+        Corrosive,
+        Irritant,
+        Sensitizer,
+        Asphyxiant
+    }
+}
